9270fdcea04c16000d867adf717f2803c3bcf7d6
rinechran <rinechran@outlook.jp>
2020-01-16 04:06:09 +0900

    feat : Actor imple...

--- Chapter03/Actor.cpp
+++ Chapter03/Actor.cpp
@@ -1,1 +1,28 @@
 #include "Actor.h"
+#include "Game.h"
+
+Actor::Actor(Game* game)
+	: mState(Actor::eState::Active)
+	, mGame(game)
+	, mScale(1.0f)
+	, mRotation(0.0f)
+	, mPosition(Vector2::Zero)
+
+{
+	
+}
+
+Actor::~Actor()
+{
+}
+
+void Actor::Update(float deltaTime)
+{
+}
+
+float Actor::GetScale() { return mScale; }
+
+void Actor::SetScale(float scale) {
+	mScale = scale;
+}
+
--- Chapter03/Actor.h
+++ Chapter03/Actor.h
@@ -1,5 +1,39 @@
 #pragma once
+#include "Math.h"
+class Game;
+class Component;
+
 class Actor
 {
+public:
+	enum class eState {
+		Active,
+		Paused,
+		Dead
+	};
+
+
+	Actor(Game* game);
+	virtual ~Actor();
+
+	virtual void Update(float deltaTime);
+
+
+	float GetScale();
+	void SetScale(float scale);
+
+
+
+private:
+	
+	eState  mState;
+
+
+	// Transform
+	Vector2 mPosition;
+	float mScale;
+	float mRotation;
+
+	Game* mGame;
 };
 
