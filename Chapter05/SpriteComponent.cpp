8b2c39b665cfec032727c97dfa9511ff1e165831
rinechran <rinechran@outlook.jp>
2020-02-18 16:52:46 +0900

    feat : glDrawElements call

--- Chapter05/SpriteComponent.cpp
+++ Chapter05/SpriteComponent.cpp
@@ -1,7 +1,9 @@
+#include "SpriteComponent.h"
 #include "Texture.h"
+#include "Shader.h"
 #include "Actor.h"
 #include "Game.h"
-#include "SpriteComponent.h"
+
 
 SpriteComponent::SpriteComponent(Actor* owner, int drawOrder)
 	: Component(owner)
@@ -19,6 +21,13 @@ SpriteComponent::~SpriteComponent()
 void SpriteComponent::Draw(Shader* shader)
 {
 	if (mTexture) {
+		Matrix4 scaleMat = Matrix4::CreateScale(
+			static_cast<float>(mTexWidth),
+			static_cast<float>(mTexHeight),
+			1.0f);
+
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 	}
 }
